fffcb143ee00a33e3e12d34748cd671dde096b46
Tamby Kojak <tambykojak@gmail.com>
2020-01-13 16:20:19 -0500

    WIP

--- src/middleware/assignUser.ts
+++ src/middleware/assignUser.ts
@@ -7,6 +7,12 @@ import AppError, { ErrorTypes } from '../errors/appError'
 
 export default async (request: BaseRequest, _: Response, next: NextFunction): Promise<void> => {
   const token = request.header('authorization')
+
+  if (!token) {
+    next()
+    return
+  }
+
   const decodedIdToken = await admin.auth().verifyIdToken(token)
   const firebaseUserId = decodedIdToken.user_id
 
@@ -19,5 +25,6 @@ export default async (request: BaseRequest, _: Response, next: NextFunction): Pr
   }
 
   request.currentUser = user
+
   next()
 }
